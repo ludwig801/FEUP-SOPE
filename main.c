a5defce8a43f1e9831b37822dcd59108fd3dd66d
ludwig801 <lmaga801@gmail.com>
2014-05-24 16:41:51 +0100

    Jesus ama-te <3

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ unsigned short done = 0;
 
 int QUEUE_SIZE = 10;
 
+
 //------------------------------------------------------------------------------------------
 // Type of the circular queue elements
 typedef unsigned long QueueElem;
@@ -76,6 +77,7 @@ void queue_put(CircularQueue *q, QueueElem value)
 // Removes element at the head of queue 'q' and returns its 'value'
 QueueElem queue_get(CircularQueue *q)
 {	
+	
 	sem_wait(&(q->production));
 	QueueElem value = q->v[q->first];
 	(q->first)++;
@@ -139,8 +141,32 @@ void queue_fill(CircularQueue *q, const QueueElem n) {
 // ************************************************************************************************************************************
 
 
-CircularQueue *primes;
+QueueElem * primes;
+unsigned int p_count = 0;
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 
+void initPrimes(int size) {
+	primes = (QueueElem *) malloc(size * sizeof(QueueElem));
+}
+
+void printPrimes() {
+	unsigned int i = 0;
+	printf("====================\n");
+	for(; i < p_count; i++) {
+		printf("%lu\n", primes[i]);
+	}
+	printf("====================\n");
+}
+
+void insertPrime(QueueElem value) {
+
+	pthread_mutex_lock(&mut);
+	primes[p_count] = value;
+	//printf("Inserted: %lu, in: %d\n", value, p_count);
+	p_count++;
+	pthread_mutex_unlock(&mut);
+	
+}
 
 void *computePrimes(void *arg) {
 
@@ -150,29 +176,19 @@ void *computePrimes(void *arg) {
 
 	prime = queue_get(queue);
 
-	//printf("Thread no: %d; Prime: %lu\n", queue->number, prime);
-
 	//
 	// Stoping condition: prime > sqrt(N)
 	//
 	if(prime > (int) sqrt(N)) {
 
-		value = queue_get(queue);
-
-		while(value != 0) {
+		while((value = queue_get(queue)) != 0) {
 
 			//
-			// Insert the prime in the prime queue
+			// Insert the prime in the prime array
 			//	
-			
-			pthread_mutex_lock(&(primes->mutex));
-			queue_put(primes, value);
-			pthread_mutex_unlock(&(primes->mutex));
-
-			value = queue_get(queue);
+			insertPrime(value);
 		}
 
-		done = 1;
 	}
 	else {
 		// Creates shared queue
@@ -189,23 +205,28 @@ void *computePrimes(void *arg) {
 			// if not multiple of the last computed prime (possible new prime)
 			if((value % prime) != 0)  {
 				queue_put(new_queue, value);
-			}	
-		} while(queue_top(queue) != 0);
+			}
+		} while(value != 0);
 
 		queue_put(new_queue, 0);	/* terminator value */	
 	}
 
 	//
-	// Insert the prime in the prime queue
+	// Insert the prime in the prime array
 	//
-	pthread_mutex_lock(&(primes->mutex));
-	queue_put(primes, prime);
-	pthread_mutex_unlock(&(primes->mutex));
+	insertPrime(prime);
 
 	queue_destroy(queue);
 
-	return NULL;	
+	pthread_exit(NULL);	
 }
+
+int cmpfunc (const void * a, const void * b)
+{
+   return ( *(QueueElem*)a - *(QueueElem*)b );
+}
+
+
 
 int main( int argc, const char* argv[] )  {
 
@@ -221,37 +242,24 @@ int main( int argc, const char* argv[] )  {
 	// ===============================================
 	N = atoi(argv[1]);
 	max_primes = 1.2 * (((double)N)/log(N));
-
-	//printf("input: %d, sqrt: %f\n", N, sqrt(N));
-	//printf("max primes: %d\n", max_primes);
 	// ===============================================
 
-	queue_init(&primes, max_primes);
+	initPrimes(max_primes);
 
+	// All possible integers queue.
 	queue_init(&initial_queue, N);
-
 	queue_fill(initial_queue, N);
-
-	done = 0;
 
 	// Creates initial thread
 	pthread_create(&tids[threads], NULL, computePrimes, initial_queue);
 
-	// Waits for all primes to be inserted
-
-	int sem_value = 0;
-	do {
-		sem_getvalue(&(primes->production), &sem_value);
-	}while((sem_value < max_primes) && (!done));
-
-	//printf("Saiu.\n");
-	//printf("Sem_value: %d\n", sem_value);
-
-	//printf("<<< PRIME QUEUE >>>\n");
-
-	queue_print(primes);
-
-	//queue_destroy(primes);
+	unsigned int i = 0;
+	for(; i <= threads; i++) {
+		pthread_join(tids[i], NULL);
+	}
+	
+	qsort(primes, p_count, sizeof(QueueElem), cmpfunc);
+	printPrimes();
 
 	return 0;
 }
